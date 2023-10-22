33dfcbd3fd405b5f7685a6c7efeaa5315ad9cb1f
Md Azaz <mdazaz9026@gmail.com>
2023-10-22 13:57:24 +0530

    middleware

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 const express = require("express")
 const app = express()
 
+/** Middleware */
+
+app.use(function(req , res , next){
+    res.send("middleware");
+    next();
+})
+
 app.get('/' , function( req , res ){
     res.send("Hello World")
+})
+
+app.get('/profile' , function( req , res ){
+    res.send("Hello my profile")
 })
 
 app.listen(3000)
