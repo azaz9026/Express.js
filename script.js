086f49a1a4270b25591fbd9615b2b006d3e95a39
Md Azaz <mdazaz9026@gmail.com>
2023-10-23 20:16:52 +0530

    Static file

--- script.js
+++ script.js
@@ -2,21 +2,32 @@ const express = require('express')
 const app = express()
 
 
-/** Middleware */
+/** Middleware 
 
 app.use( function(req , res , next){
     res.send("Middleware")
     next()
 })
+*/
 
 /** Configure of Ejs */
 
 app.set('view engine', 'ejs')
 
+/** Configure of Static folder */
+
+app.use(express.static('./public'))
+
 /** Routing */
 
 app.get('/' , function(req , res){
     res.render('index')
+})
+
+/** Route Paremeter */
+
+app.get('/profile/:username' , function( req , res ){
+    res.send(`hello ${req.params.username}`)
 })
 
 
